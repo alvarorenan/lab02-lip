91582cc78b37637ec5e73f9a798226719413b5c9
alvaro.carvalho.703 <alvaro.carvalho.703@ufrn.edu.br>
2022-09-03 00:24:22 -0300

    init

--- brasileirao.py
+++ brasileirao.py
@@ -10,13 +10,11 @@ lista_rebaixados = lista_brasileirao[-4:]
 time_maior_nome = max(lista_brasileirao, key=len)
 
 lista_metade = lista_brasileirao[0:10]
-lista_metade_reversa = lista_brasileirao[10:20]
-lista_metade_reversa.reverse();
+lista_metade_reversa = lista_brasileirao[20:9:-1]
 
-print('Lista de times classificados para a Libertadores: {}'.format(lista_libertadores))
-print('\nLista de times rebaixados: {}'.format(lista_rebaixados))
-print('\nTime com maior nome: {}'.format(time_maior_nome))
-
+print(f"Lista de times que irão para a libertadores: {lista_libertadores}\n")
+print(f"Lista de times que irão para a série B: {lista_rebaixados}\n")
+print(f"Time com maior nome: {time_maior_nome}\n")
 
 for x in lista_metade:
     print(x, "X", lista_metade_reversa[lista_metade.index(x)])
